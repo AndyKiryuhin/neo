04ae09f2590fab8b0742fac0e9f7911da0b60ff7
Andy Kiryukhin <andykiryuhin@gmail.com>
2023-03-26 22:31:24 +0300

    Task3 Accomplished

--- filters.py
+++ filters.py
@@ -71,6 +71,36 @@ class AttributeFilter:
     def __repr__(self):
         return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
 
+class DateFilter(AttributeFilter):
+    """Filter close approaches by date."""
+    @classmethod
+    def get(cls, approach):
+        return approach.time.date()
+
+class DistanceFilter(AttributeFilter):
+    """Filter close approaches by distance."""
+    @classmethod
+    def get(cls, approach):
+        return approach.distance
+
+class VelocityFilter(AttributeFilter):
+    """Filter close approaches by velocity."""
+    @classmethod
+    def get(cls, approach):
+        return approach.velocity
+
+class DiameterFilter(AttributeFilter):
+    """Filter close approaches by diameter."""
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.diameter
+    
+class HazardousFilter(AttributeFilter):
+    """Filter close approaches by hazardous."""
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.hazardous
+
 
 def create_filters(date=None, start_date=None, end_date=None,
                    distance_min=None, distance_max=None,
@@ -106,9 +136,39 @@ def create_filters(date=None, start_date=None, end_date=None,
     :param hazardous: Whether the NEO of a matching `CloseApproach` is potentially hazardous.
     :return: A collection of filters for use with `query`.
     """
-    # TODO: Decide how you will represent your filters.
-    return ()
+    filters = []
 
+    if date is not None:
+        filters.append(DateFilter(operator.eq, date))
+
+    if start_date is not None:
+        filters.append(DateFilter(operator.ge, start_date))
+
+    if end_date is not None:
+        filters.append(DateFilter(operator.le, end_date))
+
+    if distance_min is not None:
+        filters.append(DistanceFilter(operator.ge, distance_min))
+
+    if distance_max is not None:
+        filters.append(DistanceFilter(operator.le, distance_max))
+
+    if velocity_min is not None:
+        filters.append(VelocityFilter(operator.ge, velocity_min))
+
+    if velocity_max is not None:
+        filters.append(VelocityFilter(operator.le, velocity_max))
+
+    if diameter_min is not None:
+        filters.append(DiameterFilter(operator.ge, diameter_min))
+
+    if diameter_max is not None:
+        filters.append(DiameterFilter(operator.le, diameter_max))
+
+    if hazardous is not None:
+        filters.append(HazardousFilter(operator.eq, hazardous))
+
+    return filters
 
 def limit(iterator, n=None):
     """Produce a limited stream of values from an iterator.
@@ -119,5 +179,13 @@ def limit(iterator, n=None):
     :param n: The maximum number of values to produce.
     :yield: The first (at most) `n` values from the iterator.
     """
-    # TODO: Produce at most `n` values from the given iterator.
+    if n is None or n == 0:
+        yield from iterator
+    else:
+        count = 0
+        for item in iterator:
+            if count >= n:
+                break
+            yield item
+            count += 1
     return iterator
